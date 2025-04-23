c8abdbe12620f1feebe72809870dc520f344aadc
mouhamedbouzayan <mohammedbouzi177@gmail.com>
2025-04-23 16:55:04 +0100

    remove gitignore from client folder

---
+++ Client/src/pages/Articles/CreateArticle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateArticle() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateArticle
---
+++ Client/vite.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563EB",  // Bleu
+        secondary: "#059669", // Vert
+        dark: "#1E293B",     // Fond sombre
+        light: "#F8FAFC",    // Fond clair
+      },
+    },
+  },
+  plugins: [],
+}
